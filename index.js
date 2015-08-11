193efa3ac7a8f0c62fb9ff084e681e3a035d3b85
Scotty Eckenthal <scott.eckenthal@gmail.com>
2015-08-10 20:25:41 -0400

    Cleanup / consolidation

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 
 var _ = require('underscore-node'),
 	exec = require('child_process').exec,
-	program = require('commander');
+	program = require('commander'),
+	pkg = require('./package.json');
 
 var CliCmd = require('./src/cli-cmd.js')
 
-var getBranchSearchCmd = function(grepStr) {
+var getBranchSearchCmd = function(_grepStr) {
 
 	var cmd = new CliCmd();
+
+	if (this.parent && this.parent.current) {
+		grepStr = '\*';
+	} else {
+		grepStr = _grepStr;
+	}
 
 	cmd.set("git branch -l")
 		.pipe("grep '" + grepStr + "'")
@@ -18,37 +25,25 @@ var getBranchSearchCmd = function(grepStr) {
 }
 
 program
-	// .version
+	.version(pkg.version)
 	.option('-c --current', 'Current branch')
 	.option('-f --force', 'force push');
 
 program
 	.command('branch')
 	.action(function() {
-		var cmd, grepStr;
-		if (this.parent.current) {
-			cmd = getBranchSearchCmd('\*')
-		} else {
-			grepStr = this.parent.args[0];
+		var grepStr = this.parent.args[0],
 			cmd = getBranchSearchCmd(grepStr);
-		}
-
 		cmd.exec();
 	});
 
 program
 	.command('push')
 	.action(function() {
-		var cmd, grepStr;
-		if (this.parent.current) {
-			cmd = getBranchSearchCmd('\*')
-		} else {
-			grepStr = this.parent.args[0];
+		var grepStr = this.parent.args[0],
 			cmd = getBranchSearchCmd(grepStr);
-		}
 
 		var cmdStr = 'xargs git push origin';
-
 		if (this.parent.force) {
 			cmdStr += ' --force'
 		}
@@ -60,10 +55,8 @@ program
 program
 	.command('checkout')
 	.action(function() {
-		var cmd, grepStr;
-
-		grepStr = this.parent.args[0];
-		cmd = getBranchSearchCmd(grepStr);
+		var grepStr = this.parent.args[0],
+			cmd = getBranchSearchCmd(grepStr);
 
 		cmd.pipe('xargs git checkout');
 		cmd.exec();
@@ -72,10 +65,8 @@ program
 program
 	.command('merge')
 	.action(function() {
-		var cmd, grepStr;
-
-		grepStr = this.parent.args[0];
-		cmd = getBranchSearchCmd(grepStr);
+		var grepStr = this.parent.args[0],
+			cmd = getBranchSearchCmd(grepStr);
 
 		cmd.pipe('xargs git merge');
 		cmd.exec();
